4673dfa5efe395bdd09afe515bad08ff8024e49e
rassegit3 <rassegit100@gmail.com>
2025-10-24 11:58:01 +0300

    Schema-file to initialize the database

---
+++ schema.sql
@@ -0,0 +1,51 @@
+
+CREATE TABLE users(
+id INTEGER PRIMARY KEY,
+username TEXT UNIQUE,
+password_hash TEXT
+);
+CREATE TABLE star (
+id INTEGER PRIMARY KEY,
+name TEXT,
+content TEXT,
+user_id REFERENCES users);
+CREATE TABLE type (
+id INTEGER PRIMARY KEY,
+name TEXT,
+content TEXT);
+CREATE TABLE method (
+id INTEGER PRIMARY KEY,
+name  TEXT);
+CREATE TABLE planet (
+id INTEGER PRIMARY KEY,
+name TEXT,
+content TEXT,
+discovery INTEGER,
+method TEXT,
+user_id REFERENCES users);
+CREATE TABLE planet_star (
+planet_id INTEGER,
+star_id INTEGER,
+FOREIGN KEY (planet_id) REFERENCES planet(id) ON DELETE CASCADE,
+FOREIGN KEY (star_id) REFERENCES star(id) ON DELETE CASCADE);
+CREATE TABLE planet_type (
+planet_id INTEGER,
+type_id INTEGER,
+FOREIGN KEY (planet_id) REFERENCES planet(id) ON DELETE CASCADE,
+FOREIGN KEY (type_id) REFERENCES type(id));
+CREATE TABLE planet_method (
+planet_id INTEGER,
+method_id INTEGER,
+FOREIGN KEY (planet_id) REFERENCES planet(id) ON DELETE CASCADE,
+FOREIGN KEY (method_id) REFERENCES method(id));
+
+INSERt into METHOD (name) VALUES ("Suora kuvaus");
+INSERt into METHOD (name) VALUES ("Gravitaatiolinssi");
+INSERt into METHOD (name) VALUES ("Säteisnopeus");
+INSERt into METHOD (name) VALUES ("Ylikulku");
+INSERt into METHOD (name) VALUES ("Pulsarin ajoitus");
+
+INSERt into type (name) VALUES ("Kaasujättiläinen");
+INSERt into type (name) VALUES ("Maankaltainen planeetta");
+INSERt into type (name) VALUES ("Jääjättiläinen");
+INSERt into type (name) VALUES ("Kuuma Jupiter");
