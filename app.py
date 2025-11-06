6808cdd110e8a448d477ba52ee2bd69249d03923
rassegit3 <rassegit100@gmail.com>
2025-11-06 11:25:47 +0200

    Fixed planet types not being listed

--- app.py
+++ app.py
@@ -227,7 +227,9 @@ def modify():
 def create_planet():
     planet_name = request.form["planetname"]
     planet_content = request.form["planetcontent"]
-    planet_types = request.form.getlist("planettypes[]")
+    planet_types = request.form.getlist('planettypes')
+
+    print(planet_types)
 
     planet_star = request.form["planetstar"]
     planet_date = request.form["planetdate"]
@@ -248,11 +250,12 @@ def create_planet():
     result = db.query(sql, [planet_name])[0]
     planet_id = int(result[0])
 
-    print(planet_types)
+    
+    
 
     for item in planet_types:
         sql = ("""SELECT id FROM type WHERE name = ?""")
-        result = db.query(sql, [item])[0]
+        result = db.query(sql, [item])[0][0]
         planet_type_id = int(result)
         sql = ("INSERT INTO planet_type (planet_id, type_id) VALUES (?,?)")
         db.execute(sql, [planet_id, planet_type_id])
